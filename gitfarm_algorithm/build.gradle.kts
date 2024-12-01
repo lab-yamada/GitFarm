994a960743036dd1926bbddaa121b32e995b9f5d
reidlo5135 <201840103@email.daelim.ac.kr>
2024-12-01 14:36:54 +0900

    refactor : modify gradle

--- gitfarm_algorithm/build.gradle.kts
+++ gitfarm_algorithm/build.gradle.kts
@@ -17,6 +17,15 @@ tasks.jar {
     manifest {
         attributes["Main-Class"] = "MainKt"
     }
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
 
 tasks.test {
