0a117d342dc6db049fad4589ca270fb2c774bf59
reidlo5135 <201840103@email.daelim.ac.kr>
2024-12-12 10:52:34 +0900

    chore : add gradle task.jar

--- gitfarm_middleware/build.gradle.kts
+++ gitfarm_middleware/build.gradle.kts
@@ -14,6 +14,7 @@ val ktor_version: String by project;
 val logback_version: String by project
 
 dependencies {
+    implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
@@ -31,6 +32,21 @@ sourceSets {
             srcDir("src/main/resources")
         }
     }
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
 
 tasks.withType<ProcessResources> {
